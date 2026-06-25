9648eee0ca7363fd33bfea3f1346f310a3f1ffc6
yokatta <tolyann358@gmail.com>
2026-06-25 07:54:47 +0000

    fix: clang-tidy fixes

--- project/logger.cpp
+++ project/logger.cpp
@@ -1,26 +1,32 @@
 #include "logger.h"
-#include <spdlog/spdlog.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
 
-struct Logger::Impl {
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+
+struct Logger::Impl
+{
     std::shared_ptr<spdlog::logger> logger;
 };
 
-Logger::Logger() : impl_(new Impl) {
+Logger::Logger() : impl_(std::make_unique<Impl>())
+{
     impl_->logger = spdlog::stdout_color_mt("calculator");
 }
 
 Logger::~Logger() = default;
 
-Logger& Logger::getInstance() {
+Logger& Logger::getInstance()
+{
     static Logger instance;
     return instance;
 }
 
-void Logger::info(const std::string& message) {
+void Logger::info(const std::string& message)
+{
     impl_->logger->info(message);
 }
 
-void Logger::error(const std::string& message) {
+void Logger::error(const std::string& message)
+{
     impl_->logger->error(message);
 }
--- project/main.cpp
+++ project/main.cpp
@@ -1,13 +1,17 @@
-#include <stdio.h>
-#include <string>
-#include <iostream>
-#include "math_lib.h"
-#include <nlohmann/json.hpp>
 #include "logger.h"
+#include "math_lib.h"
+
 #include <libpq-fe.h>
-#include <optional>
-#include <unordered_map>
+
+#include <nlohmann/json.hpp>
+
+#include <cstdio>
+#include <iostream>
 #include <memory>
+#include <optional>
+#include <span>
+#include <string>
+#include <unordered_map>
 
 namespace calculator
 {
@@ -22,121 +26,149 @@ struct Task
     bool show_help{false};
 };
 
-struct PGconnDeleter 
+struct PGconnDeleter
 {
-    void operator()(PGconn* conn) const 
+    void operator()(PGconn* conn) const
     {
-        if (conn) PQfinish(conn);
+        if (conn != nullptr)
+        {
+            PQfinish(conn);
+        }
     }
 };
 
-struct PGresultDeleter 
+struct PGresultDeleter
 {
-    void operator()(PGresult* res) const 
+    void operator()(PGresult* res) const
     {
-        if (res) PQclear(res);
+        if (res != nullptr)
+        {
+            PQclear(res);
+        }
     }
 };
 
 using PGresultPtr = std::unique_ptr<PGresult, PGresultDeleter>;
 
-class DataBase 
+class DataBase
 {
-public:
-    struct Config 
+  public:
+    struct Config
     {
         std::string host{"localhost"};
         std::string port{"5432"};
-        std::string username{"calculator_user"}; 
+        std::string username{"calculator_user"};
         std::string password{"iwillwin"};
         std::string dbname{"calculator"};
     };
 
-    DataBase() : connection_(nullptr) {}
+    DataBase() : connection_(nullptr)
+    {}
 
-    ~DataBase() { disconnect(); }
-
-    void connect() 
+    ~DataBase()
     {
-        std::string connStr = 
-            "host=" + config_.host +
-            " port=" + config_.port +
-            " user=" + config_.username +
-            " password=" + config_.password +
+        disconnect();
+    }
+
+    DataBase(const DataBase&) = delete;
+    DataBase& operator=(const DataBase&) = delete;
+
+    DataBase(DataBase&&) = default;
+    DataBase& operator=(DataBase&&) = default;
+
+    void connect()
+    {
+        const std::string connStr =
+            "host=" + config_.host + " port=" + config_.port +
+            " user=" + config_.username + " password=" + config_.password +
             " dbname=" + config_.dbname;
 
         connection_.reset(PQconnectdb(connStr.c_str()));
 
-        if (PQstatus(connection_.get()) != CONNECTION_OK) 
+        if (PQstatus(connection_.get()) != CONNECTION_OK)
         {
-            std::string err = PQerrorMessage(connection_.get());
+            const std::string err = PQerrorMessage(connection_.get());
             connection_.reset();
             throw std::runtime_error("DB connection failed: " + err);
         }
     }
 
-    void disconnect() 
+    void disconnect()
     {
-        connection_.reset(); 
+        connection_.reset();
     }
 
-    std::optional<Task> getRecord(const Task& task) 
+    std::optional<Task> getRecord(const Task& task)
     {
-        std::string key = makeKey(task);
-        auto it = cache_.find(key);
-        if (it != cache_.end())
-            return it->second;
+        const std::string key = makeKey(task);
+        auto iter = cache_.find(key);
+        if (iter != cache_.end())
+        {
+            return iter->second;
+        }
         return std::nullopt;
     }
 
-    std::string makeKey(const Task& task) const 
+    static std::string makeKey(const Task& task)
     {
-        int a = task.first_num;
-        int b = task.second_num;
-        if ((task.operation == '+' || task.operation == '*') && a > b)
-            std::swap(a, b);
-        return std::to_string(a) + task.operation + std::to_string(b);
+        int firstNum = task.first_num;
+        int secondNum = task.second_num;
+        if ((task.operation == '+' || task.operation == '*') &&
+            firstNum > secondNum)
+        {
+            std::swap(firstNum, secondNum);
+        }
+        return std::to_string(firstNum) + task.operation +
+               std::to_string(secondNum);
     }
 
-    void writeRecord(const Task& task) 
+    void writeRecord(const Task& task)
     {
         cache_[makeKey(task)] = task;
 
-        std::string query = 
-            "INSERT INTO operations (first_num, second_num, operation, result, status) VALUES (" +
+        const std::string query =
+            "INSERT INTO operations (first_num, second_num, "
+            "operation, result, status) VALUES (" +
             std::to_string(task.first_num) + ", " +
-            std::to_string(task.second_num) + ", '" +
-            task.operation + "', " +
-            std::to_string(task.result) + ", " +
-            std::to_string(task.status) + ")";
+            std::to_string(task.second_num) + ", '" + task.operation + "', " +
+            std::to_string(task.result) + ", " + std::to_string(task.status) +
+            ")";
 
-        PGresultPtr res(PQexec(connection_.get(), query.c_str()));
-        if (PQresultStatus(res.get()) != PGRES_COMMAND_OK) 
-            throw std::runtime_error("writeRecord failed: " + std::string(PQerrorMessage(connection_.get())));
-    }
-
-    void warmUpCach() {
-        PGresultPtr res(PQexec(connection_.get(), 
-            "SELECT first_num, second_num, operation, result, status FROM operations"));
-        
-        if (PQresultStatus(res.get()) != PGRES_TUPLES_OK) 
-            throw std::runtime_error("warmUpCach failed");
-
-        int rows = PQntuples(res.get());
-        for (int i = 0; i < rows; i++) 
+        const PGresultPtr res(PQexec(connection_.get(), query.c_str()));
+        if (PQresultStatus(res.get()) != PGRES_COMMAND_OK)
         {
-            Task t;
-            t.first_num  = std::stoi(PQgetvalue(res.get(), i, 0));
-            t.second_num = std::stoi(PQgetvalue(res.get(), i, 1));
-            t.operation  = PQgetvalue(res.get(), i, 2)[0];
-            t.result     = std::stoi(PQgetvalue(res.get(), i, 3));
-            t.status     = std::stoi(PQgetvalue(res.get(), i, 4));
-            cache_[makeKey(t)] = t;
+            throw std::runtime_error(
+                "writeRecord failed: " +
+                std::string(PQerrorMessage(connection_.get())));
         }
     }
 
+    void warmUpCach()
+    {
+        const PGresultPtr res(PQexec(connection_.get(),
+                                     "SELECT first_num, second_num, operation, "
+                                     "result, status FROM operations"));
 
-private:
+        if (PQresultStatus(res.get()) != PGRES_TUPLES_OK)
+        {
+            throw std::runtime_error("warmUpCach failed");
+        }
+
+        const int rows = PQntuples(res.get());
+        for (int i = 0; i < rows; i++)
+        {
+            Task task;
+            task.first_num = std::stoi(PQgetvalue(res.get(), i, 0));
+            task.second_num = std::stoi(PQgetvalue(res.get(), i, 1));
+            const std::string opStr = PQgetvalue(res.get(), i, 2);
+            task.operation = opStr.at(0);
+            task.result = std::stoi(PQgetvalue(res.get(), i, 3));
+            task.status = std::stoi(PQgetvalue(res.get(), i, 4));
+            cache_[makeKey(task)] = task;
+        }
+    }
+
+  private:
     Config config_;
     std::unique_ptr<PGconn, PGconnDeleter> connection_;
     std::unordered_map<std::string, Task> cache_;
@@ -144,135 +176,159 @@ private:
 
 class Application
 {
-    public:
-        Application()
+  public:
+    Application()
+    {
+        dataBase_.connect();
+        dataBase_.warmUpCach();
+    }
+
+    ~Application()
+    {
+        dataBase_.disconnect();
+    }
+
+    Application(const Application&) = delete;
+    Application& operator=(const Application&) = delete;
+
+    Application(Application&&) = delete;
+    Application& operator=(Application&&) = delete;
+
+    void run(int argc, char** argv)
+    {
+        getTask(argc, argv);
+        if (task_.show_help)
         {
-            dataBase_.connect();
-            dataBase_.warmUpCach();
+            printHelp();
+            return;
         }
 
-        ~Application()
+        std::optional<Task> dbRecord = dataBase_.getRecord(task_);
+        if (!dbRecord)
         {
-            dataBase_.disconnect();
-        }
-        void run(int argc, char** argv)
-        {
-            getTask(argc, argv);
-            if (task_.show_help)
+            Logger::getInstance().info("Cache miss, calculating...");
+            try
             {
-                printHelp();
-                return;
+                makeCalculate();
+                task_.status = 0;
             }
-
-            std::optional<Task> dbRecord = dataBase_.getRecord(task_);
-            if (!dbRecord)
+            catch (const std::overflow_error& e)
             {
-                Logger::getInstance().info("Cache miss, calculating...");
-                try
-                {
-                    makeCalculate();
-                    task_.status = 0;
-                }
-                catch(const std::overflow_error& e)
-                {
-                    Logger::getInstance().error(e.what());
-                    task_.result = 0;
-                    task_.status = 1;
-                }
-                catch (const std::runtime_error& e)
-                {
-                    Logger::getInstance().error(e.what());
-                    task_.result = 0;
-                    task_.status = 2;                    
-                }
-                dataBase_.writeRecord(task_);
+                Logger::getInstance().error(e.what());
+                task_.result = 0;
+                task_.status = 1;
             }
-            else
+            catch (const std::runtime_error& e)
             {
-                Logger::getInstance().info("Cache hit!");
-                task_ = *dbRecord;
+                Logger::getInstance().error(e.what());
+                task_.result = 0;
+                task_.status = 2;
             }
-            printResult();
+            dataBase_.writeRecord(task_);
         }
-    private:
-        void printHelp() 
+        else
         {
-            printf("Usage:\n");
-            printf("  calculator '{\"a\":5,\"b\":3,\"op\":\"+\"}'\n\n");
-
-            printf("JSON fields:\n");
-            printf("  a   First integer number\n");
-            printf("  b   Second integer number\n");
-            printf("  op  Operation\n\n");
-
-            printf("Operations:\n");
-            printf("  +   Addition\n");
-            printf("  -   Subtraction\n");
-            printf("  *   Multiplication\n");
-            printf("  /   Division\n");
-            printf("  ^   Power\n");
-            printf("  !   Factorial\n\n");
-
-            printf("Examples:\n");
-            printf("  calculator '{\"a\":5,\"b\":3,\"op\":\"+\"}'\n");
-            printf("  calculator '{\"a\":10,\"b\":2,\"op\":\"/\"}'\n");
-            printf("  calculator '{\"a\":5,\"op\":\"!\"}'\n");
+            Logger::getInstance().info("Cache hit!");
+            task_ = *dbRecord;
         }
-        void getTask(int argc, char** argv)
+        printResult();
+    }
+
+  private:
+    static void printHelp()
+    {
+        std::cout << "Usage:\n";
+        std::cout << "  calculator '{\"a\":5,\"b\":3,\"op\":\"+\"}'\n\n";
+
+        std::cout << "JSON fields:\n";
+        std::cout << "  a   First integer number\n";
+        std::cout << "  b   Second integer number\n";
+        std::cout << "  op  Operation\n\n";
+
+        std::cout << "Operations:\n";
+        std::cout << "  +   Addition\n";
+        std::cout << "  -   Subtraction\n";
+        std::cout << "  *   Multiplication\n";
+        std::cout << "  /   Division\n";
+        std::cout << "  ^   Power\n";
+        std::cout << "  !   Factorial\n\n";
+
+        std::cout << "Examples:\n";
+        std::cout << "  calculator '{\"a\":5,\"b\":3,\"op\":\"+\"}'\n";
+        std::cout << "  calculator '{\"a\":10,\"b\":2,\"op\":\"/\"}'\n";
+        std::cout << "  calculator '{\"a\":5,\"op\":\"!\"}'\n";
+    }
+    void getTask(int argc, char** argv)
+    {
+        if (argc < 2)
         {
-            if (argc < 2)
-                throw std::runtime_error("No input provided. Use --help for usage.");
-
-            if (std::string(argv[1]) == "--help" || std::string(argv[1]) == "-h") 
-            {
-                task_.show_help = true;
-                return;
-            }
-            nlohmann::json j = nlohmann::json::parse(argv[1]); 
-            task_.first_num  = j.at("a").get<int>();
-            task_.second_num = j.value("b", 0);
-            task_.operation  = j.at("op").get<std::string>()[0];
-        }
-        void makeCalculate()
-        {
-            switch (task_.operation)
-            {
-                case '+':
-                    math_lib::Math::calculateSum(task_.first_num, task_.second_num, task_.result);
-                    break;
-                case '-':
-                    math_lib::Math::calculateDifference(task_.first_num, task_.second_num,
-                                                        task_.result);
-                    break;
-                case '*':
-                    math_lib::Math::calculateMult(task_.first_num, task_.second_num, task_.result);
-                    break;
-                case '/':
-                     math_lib::Math::calculateDivision(task_.first_num, task_.second_num,
-                                                        task_.result);
-                    break;
-                case '^':
-                    math_lib::Math::calculatePower(task_.first_num, task_.second_num, task_.result);
-                    break;
-                case '!':
-                    math_lib::Math::calculateFactorial(task_.first_num, task_.result);
-                    break;
-            }
-        }
-        void printResult() const
-        {
-            if (task_.status == 0)
-                printf("%d\n", task_.result);
-            else
-                printf("Error: operation failed with status %d\n", task_.status);
+            throw std::runtime_error(
+                "No input provided. Use --help for usage.");
         }
 
-    private:
-        Task task_;    
-        DataBase dataBase_;
+        const std::span<char*> args(argv, static_cast<std::size_t>(argc));
+        const std::string arg(args[1]);
+
+        if (arg == "--help" || arg == "-h")
+        {
+            task_.show_help = true;
+            return;
+        }
+        nlohmann::json json = nlohmann::json::parse(args[1]);
+        task_.first_num = json.at("a").get<int>();
+        task_.second_num = json.value("b", 0);
+        task_.operation = json.at("op").get<std::string>()[0];
+    }
+    void makeCalculate()
+    {
+        switch (task_.operation)
+        {
+            case '+':
+                math_lib::Math::calculateSum(task_.first_num, task_.second_num,
+                                             task_.result);
+                break;
+            case '-':
+                math_lib::Math::calculateDifference(
+                    task_.first_num, task_.second_num, task_.result);
+                break;
+            case '*':
+                math_lib::Math::calculateMult(task_.first_num, task_.second_num,
+                                              task_.result);
+                break;
+            case '/':
+                math_lib::Math::calculateDivision(
+                    task_.first_num, task_.second_num, task_.result);
+                break;
+            case '^':
+                math_lib::Math::calculatePower(task_.first_num,
+                                               task_.second_num, task_.result);
+                break;
+            case '!':
+                math_lib::Math::calculateFactorial(task_.first_num,
+                                                   task_.result);
+                break;
+            default:
+                throw std::runtime_error("Unknown operation");
+        }
+    }
+    void printResult() const
+    {
+        if (task_.status == 0)
+        {
+            std::cout << task_.result << '\n';
+        }
+        else
+        {
+            std::cout << "Error: operation failed with status " << task_.status
+                      << '\n';
+        }
+    }
+
+    Task task_;
+    DataBase dataBase_;
 };
 
-}
+} // namespace calculator
 
 int main(int argc, char* argv[])
 {
@@ -281,10 +337,10 @@ int main(int argc, char* argv[])
         calculator::Application application;
         application.run(argc, argv);
     }
-    catch (const nlohmann::json::exception& e) 
+    catch (const nlohmann::json::exception& e)
     {
         Logger::getInstance().error(e.what());
-        std::cout << "Error: JSON parse error: " << e.what() << std::endl;
+        std::cout << "Error: JSON parse error: " << e.what() << '\n';
     }
     catch (const std::exception& e)
     {
